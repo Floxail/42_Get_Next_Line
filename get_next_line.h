35701701c3a880adc6a285c4f5b287888068bd0a
Florent Vejux <flvejux@c1r9s7.42lausanne.ch>
2025-10-18 09:47:03 +0200

    update .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: flvejux <flvejux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 08:41:33 by flvejux           #+#    #+#             */
-/*   Updated: 2025/10/17 09:32:53 by flvejux          ###   ########.fr       */
+/*   Updated: 2025/10/18 09:46:34 by flvejux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *str);
 
 #endif
